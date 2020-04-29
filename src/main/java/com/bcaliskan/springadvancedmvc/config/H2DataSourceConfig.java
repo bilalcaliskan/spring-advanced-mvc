d86b67ecf8537c8f81701ef2b102359b9de02803
bilalcaliskan <bilalcaliskan@protonmail.com>
2020-04-29 12:53:00 +0300

    remove transaction management auto configuration

--- src/main/java/com/bcaliskan/springadvancedmvc/config/H2DataSourceConfig.java
+++ src/main/java/com/bcaliskan/springadvancedmvc/config/H2DataSourceConfig.java
@@ -3,9 +3,14 @@ package com.bcaliskan.springadvancedmvc.config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 @Configuration
-public class H2DataSourceConfig {
+@EnableTransactionManagement
+public class H2DataSourceConfig implements TransactionManagementConfigurer {
 
     @Bean
     public DataSource dataSource() {
@@ -15,6 +20,11 @@ public class H2DataSourceConfig {
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
+    }
+
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource());
     }
 
 }
